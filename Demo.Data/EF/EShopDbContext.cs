9dab1481b861445821807ffa8ce64e59073cff1b
Your name here <khoitr190@gmail.com>
2020-10-19 00:41:17 +0700

    add EF and Entity

---
+++ Demo.Data/EF/EShopDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Demo.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Demo.Data.EF
+{
+    public class EShopDbContext : DbContext
+    {
+        public EShopDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
