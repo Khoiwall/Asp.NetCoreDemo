a67a6aa3013427451db6d2f18d19d51b0ee2fffa
Your name here <khoitr190@gmail.com>
2020-10-24 11:19:30 +0700

    .

--- Demo.Data/EF/EShopDbContext.cs
+++ Demo.Data/EF/EShopDbContext.cs
@@ -65,9 +65,5 @@ namespace eShopSolution.Data.EF
         public DbSet<Promotion> Promotions { get; set; }
 
         public DbSet<Transaction> Transactions { get; set; }
-
-        public DbSet<ProductImage> ProductImages { get; set; }
-
-        public DbSet<Slide> Slides { get; set; }
     }
 }
